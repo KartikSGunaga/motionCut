55be7d9af81df9667bfbc44da629c596f42052d0
KartikSGunaga <kartik.s@learner.manipal.edu>
2023-12-11 23:11:50 +0530

    Initial commit

---
+++ week01/PasswordGenerator.py
@@ -0,0 +1,76 @@
+import string as st
+import random as rd
+
+
+def printPassword(password):
+    print(f"Kartik's Recommended password is: \n{password}")
+    return None
+
+
+def chooseLength(choice):
+    if choice == "yes":
+        # If the user chooses to specify the password length
+        num = int(input("Enter the number of digits you want: "))
+        letters = int(input("Enter the number of letters you want: "))
+        spChar = int(input("Enter the number of special characters you want: "))
+        generatePassword(num, letters, spChar)
+
+    else:
+        # If we choose to use a default password length
+        length = 17
+        num = rd.randint(1, length - 4)  # Ensuring at least one digit
+        length -= num
+        letters = rd.randint(1, length - 3)  # Ensuring at least one letter
+        spChar = length - letters  # All remaining characters are special characters
+        generatePassword(num, letters, spChar)
+
+
+def generatePassword(num, letters, spChar):
+    password = ''
+    length = num + letters + spChar
+
+    while True:
+        if length == 0:
+            break
+
+        else:
+            randomChoice = rd.choice(["Num", "Letters", "SpChars"])
+
+            if randomChoice == "Num" and num > 0:
+                password += str(rd.choice(st.digits))
+                num -= 1
+                length -= 1
+
+            elif randomChoice == "Letters" and letters > 0:
+                password += str(rd.choice(st.ascii_letters))
+                letters -= 1
+                length -= 1
+
+            elif randomChoice == "SpChars" and spChar > 0:
+                password += str(rd.choice(st.punctuation))
+                spChar -= 1
+                length -= 1
+
+    printPassword(password)
+
+
+def main():
+    print("\nWelcome to Kartik's Password Generator!")
+
+    while True:
+        # Get user input for choosing the password length
+        response = input("\nDo you wish to define password length (yes or no): ").lower()
+
+        # Call the function based on user's choice
+        chooseLength(response)
+
+        choice = input("\nDo you want another password to be generated? (yes or no): ")
+        if choice.lower() == "yes":
+            continue
+        else:
+            print("\nThank you for using Kartik's Password Generator!\nHope it helped :) ")
+            break
+
+
+if __name__ == "__main__":
+    main()
